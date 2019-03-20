b185b1dc1601ac4e5d7861c4cc9a7d86572c5c42
Arnaud <everywhere.work@outlook.fr>
2019-03-20 16:20:19 +0100

    add conf files

---
+++ webSite/inclassables/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Configuration for database connection // I dont care 
+ *
+ */
+
+$host       = "localhost";
+$username   = "root";
+$password   = ""; 
+$dbname     = "db_test";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
